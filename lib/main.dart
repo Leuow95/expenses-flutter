89a91dd7ecf878d5e8aede978be87f87bf410c09
Leuow <lmdl.eng@uea.edu.br>
2020-11-19 18:20:03 -0400

    refactoring the transaction_form

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,6 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-  String value;
-
   final _transactions = [
     Transaction(
       id: 't1',
@@ -49,42 +45,6 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           TransactionList(_transactions),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(
-                      labelText: 'Título',
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      FlatButton(
-                        child: Text('Nova Transação'),
-                        textColor: Colors.white,
-                        color: Colors.purple,
-                        onPressed: () {
-                          print(titleController.text);
-                          print(valueController.text);
-                        },
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          )
         ],
       ),
     );
