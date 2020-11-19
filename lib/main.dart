6ad44f1f4fafb28d35cd7776c54874e27331ca9d
Leuow <lmdl.eng@uea.edu.br>
2020-11-18 22:48:39 -0400

    importing and implementing the intl do formatDate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -61,7 +62,7 @@ class MyHomePage extends StatelessWidget {
                       )),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tr.value.toString(),
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -79,7 +80,7 @@ class MyHomePage extends StatelessWidget {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        Text(tr.date.toString(),
+                        Text(DateFormat('d MMM y').format(tr.date),
                             style: TextStyle(
                               color: Colors.grey[600],
                             )),
